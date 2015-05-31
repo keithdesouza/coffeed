729d044a2b5a0bf6c60097db03ec251239ae3df4
Keith de Souza <keith.desouza@genesisgroupuk.com>
2015-05-31 20:30:40 +0100

    Preparing for Heroku

--- coffeed/settings.py
+++ coffeed/settings.py
@@ -87,6 +87,19 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
 
+# =======================================
+# Next few lines added for "Getting started with Django on Heroku" 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+# =======================================
+
 LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'UTC'
